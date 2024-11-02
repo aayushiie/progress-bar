cb2f119c64a072a9fb415c5498e1f6f5012c007c
Aayushi Singh <aayushi.singh067@gmail.com>
2024-11-02 17:52:34 +0530

    added feature to update width of progress bar

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
 const progressBar = document.querySelector('.inner-box');
 const containerBox = document.querySelector('.outer-box');
 
-//counting the number of checked boxes
+let completedTasks = 0;
+
 function countCheckedBoxes() {
     let count = 0;
-
-
-    console.log(`count initially = ${count}`)
-
-
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
+    //Count checked boxes
     for (let i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
             count++;
         }
     }
 
+    //Percentage of completed tasks
+    completedTasks = Math.trunc((count / checkboxes.length) * 100);
 
-
-    console.log(`count finally: ${count}`);
-    console.log(`number of checkboxes: ${checkboxes.length}`)
-
-
-
+    //Update the width of progress bar
+    progressBar.style.width = completedTasks + '%';
 }
-countCheckedBoxes()
 
 //updating values of checked boxes
 const checkboxes = document.querySelectorAll('.my-checkbox');
-checkboxes.forEach(checkbox =>{
+checkboxes.forEach(checkbox => {
     checkbox.addEventListener('change', countCheckedBoxes)
 })
-console.log(`checked checkboxes: ${checkboxes}`); //for verification
 
-//percentage of completed tasks (here 3 tasks)
-const completedTasks = count/3*100;
-console.log(completedTasks);
+// console.log(`checked checkboxes: ${checkboxes}`); //for verification
